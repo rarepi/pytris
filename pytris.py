2e1eda06a95e8107b1d2a231617c57f1f70e3ee4
Shiki <elpingu42@gmail.com>
2022-10-29 19:41:11 +0200

    Stylize game; Implement collision check (and dodge) on rotation; Display preview of upcoming block

--- pytris.py
+++ pytris.py
@@ -9,9 +9,13 @@ from random import choice
 
 BOARD_WIDTH = 10
 BOARD_HEIGHT = 20
+INITIAL_SPEED = 1
+MAX_SPEED = 3
 
+
+clear = lambda: os.system('cls')    # windows only, os.system('clear') on linux
+clear()
 #os.system('')      # any call to os.system() is necessary for the renderer to work correctly
-os.system('cls')    # windows only, os.system('clear') on linux
 
 def overwrite(lines):
     print("\x1B[" + str(lines) + "F")
@@ -38,9 +42,16 @@ def get_random_block_type():
 class Score():
     def __init__(self):
         self.points = 0
+        self.scoring = {
+            1: 40,
+            2: 100,
+            3: 300,
+            4: 1200
+        }
 
-    def row_completed(self):
-        self.points += 1000
+    def rows_completed(self, amount):
+        if amount in self.scoring:
+            self.points += self.scoring[amount]
 
 class Board(): # TODO maybe extend numpy array?
     def __init__(self, width: int, height: int):
@@ -48,9 +59,16 @@ class Board(): # TODO maybe extend numpy array?
         self.height = height
         self.array = np.zeros((height, width))
         self.block = Block(get_random_block_type(), self)
-        self.gravity = RepeatedTimer(1, self.move, Direction.DOWN)
-        self.score = Score()
+        self.block_next = Block(get_random_block_type(), self)
         self.gameover = False
+        self.score = Score()
+        self.set_gravity(INITIAL_SPEED)
+        self.pause_renderer = False
+
+    def set_gravity(self, g):
+        if g > MAX_SPEED:
+            g = MAX_SPEED
+        self.gravity = RepeatedTimer(1 / g, self.move, Direction.DOWN)
 
     def move(self, direction):
         self.block.move(direction)
@@ -67,21 +85,52 @@ class Board(): # TODO maybe extend numpy array?
         self.gravity.stop()
         x0 = self.block.pos[0]
         y0 = self.block.pos[1]
+        # write block onto board
         for i in range(self.block.array.shape[0]):
             for j in range(self.block.array.shape[1]):
                 self.array[y0 + i][x0 + j] += self.block.array[i][j]
+
+        # another vertical loop to check for finished rows
+        rows_completed = 0
+        for i in range(self.block.array.shape[0]):
             if not self.gameover and np.all(self.array[y0 + i] == 1):
-                self.score.row_completed()
                 self.remove_row(y0 + i)
+                rows_completed += 1
+        if rows_completed > 0:
+            self.score.rows_completed(rows_completed)
+            self.set_gravity(INITIAL_SPEED + self.score.points * 0.00033)
+
         if not self.gameover:
-            self.block = Block(get_random_block_type(), self)
+            self.block = self.block_next
+            self.block_next = Block(get_random_block_type(), self)
+            # check if new block is stuck already
+            if self.block.detect_collision(None):
+                self.finalize_block()
             self.gravity.restart()
 
     def render(self):
+        if self.pause_renderer:
+            return
+
+        def format_array_str(array):
+            char_replacements = {
+                ".": "",
+                " ": "",
+                "0": " ",
+                "1": "X",
+                "[": "|",
+                "]": "|",
+                "2": "#",
+                "3": "#",
+                "4": "#"
+            }
+            result = str(array)
+            for key in char_replacements:
+                result = result.replace(key, char_replacements[key])
+            return result
+            
         CURRENT_BLOCK_DISPLAY_HEIGHT = 6
-        BOARD_DISPLAY_WIDTH = str(self.array[0]).__len__()
-        BOARD_DISPLAY_HEIGHT = self.array.shape[0]
-        GAME_INFO_DISPLAY_HEIGHT = CURRENT_BLOCK_DISPLAY_HEIGHT + 3     # initial line + score + input line
+        BOARD_DISPLAY_WIDTH = format_array_str(self.array[0]).__len__()
 
         result = ""
 
@@ -93,25 +142,25 @@ class Board(): # TODO maybe extend numpy array?
             for j in range(self.block.array.shape[1]):
                 display_array[y0 + i][x0 + j] += self.block.array[i][j]
 
-
         # render game info
         if self.gameover:
             result += "### GAME OVER ###\n"
         result += "Score: " + str(self.score.points) + "\n"
+        result += "Speed: " + "%.2f" % (1 / self.gravity.interval) + " bps\n"
 
-        # render current block
-        block_display_vpadding = CURRENT_BLOCK_DISPLAY_HEIGHT - self.block.array.shape[0]
-        block_display_hpadding = ceil(BOARD_DISPLAY_WIDTH/2) - self.block.array.shape[1]
+        # render upcoming block
+        block_display_vpadding = CURRENT_BLOCK_DISPLAY_HEIGHT - self.block_next.array.shape[0]
+        block_display_hpadding = ceil(BOARD_DISPLAY_WIDTH/2) - self.block_next.array.shape[1]
         for i in range( floor(block_display_vpadding/2) ):
             result += " "*BOARD_DISPLAY_WIDTH + "\n"
-        for i in range(self.block.array.shape[0]):
-            result += " "*block_display_hpadding + str(self.block.array[i]) + " "*block_display_hpadding + "\n"
+        for i in range(self.block_next.array.shape[0]):
+            result += " "*block_display_hpadding + format_array_str(self.block_next.array[i]) + " "*block_display_hpadding + "\n"
         for i in range( ceil(block_display_vpadding/2) ):
             result += " "*BOARD_DISPLAY_WIDTH + "\n"
 
         # render current board
         for i in range(display_array.shape[0]):
-            result += str(display_array[i]) + "\n"
+            result += format_array_str(display_array[i]) + "\n"
 
         # reposition cursor
         overwrite(result.count('\n') + 2)   # +2 for initial empty line and console input line
@@ -121,16 +170,21 @@ class Block: # TODO maybe extend numpy array?
     def __init__(self, array: np.ndarray, board: Board):
         self.array = array
         self.board = board
-        self.board.block = self
         self.pos = [round(board.width/2)-round(self.array.shape[1]/2), 0]    # x,y
 
     def rotate(self):
         if self.board.gameover:
             return
-        self.array = np.rot90(self.array)
-        # move block back onto board if rotation moved parts of it out of bounds
-        while self.array.shape[1]+self.pos[0] > self.board.width:
-            self.pos[0] -= 1
+        if not self.detect_collision(None, True):
+            self.array = np.rot90(self.array)
+        else:
+            for direction in Direction:
+                if not self.detect_collision(direction, True):
+                    self.board.pause_renderer = True
+                    self.array = np.rot90(self.array)
+                    self.move(direction)
+                    self.board.pause_renderer = False
+                    break
         self.board.render()
 
     def move(self, direction):
@@ -153,14 +207,18 @@ class Block: # TODO maybe extend numpy array?
                     self.pos[0] = self.pos[0] - 1
         self.board.render()
 
-    def detect_collision(self, direction):
-        block = self.array
-        x0 = self.pos[0]
-        x1 = x0 + block.shape[1]
-        y0 = self.pos[1]
-        y1 = y0 + block.shape[0]
+    def detect_collision(self, move_direction, rotate = False):
+        if rotate:
+            block_array = np.rot90(self.array)
+        else:
+            block_array = self.array
 
-        match direction:
+        x0 = self.pos[0]
+        x1 = x0 + block_array.shape[1]
+        y0 = self.pos[1]
+        y1 = y0 + block_array.shape[0]
+
+        match move_direction:
             case Direction.UP:
                 y0 -= 1
                 y1 -= 1
@@ -176,7 +234,14 @@ class Block: # TODO maybe extend numpy array?
             case _:
                 None
 
-        overlap = block + self.board.array[y0:y1, x0:x1]
+        if x1 > self.board.array.shape[1] or y1 > self.board.array.shape[0]:
+            return True
+
+        try:
+            overlap = block_array + self.board.array[y0:y1, x0:x1]
+        except ValueError:
+            return True
+
         if np.any(overlap[:, :] > 1):
             return True
         else:
@@ -205,6 +270,9 @@ def main():
                     board.block.move(Direction.RIGHT)
                 case 'r':
                     board.block.rotate()
+                case 'c':
+                    clear()
+                    board.render()
                 case _:
                     None
 
